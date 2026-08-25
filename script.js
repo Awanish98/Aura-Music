3865fad282165a68f1c5d54e8d9953e5a5f62063
Aura Music Admin <admin@auramusic.local>
2026-08-25 20:16:53 +0530

    feat(clean): v105.0 - Dedicated Jam Room modal only (stripped EQ, Sleep Timer & Mini-Player tabs)

--- script.js
+++ script.js
@@ -191,7 +191,7 @@
   if ('caches' in window) {
     caches.keys().then(function (names) {
       names.forEach(function (name) {
-        if (name !== 'aura-music-v104.0') caches.delete(name);
+        if (name !== 'aura-music-v105.0') caches.delete(name);
       });
     });
   }
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'aura-music-v104.0';
+var CACHE_NAME = 'aura-music-v105.0';
 
 
 
